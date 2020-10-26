809a9fb59aab78e0ee61ccbd9bffbcb86ab16a38
ForestF0X <erikopylov@gmail.com>
2020-10-26 12:14:08 +0500

    ???

--- app/src/main/java/com/eric/zvonilka/activities/LaunchActivity.kt
+++ app/src/main/java/com/eric/zvonilka/activities/LaunchActivity.kt
@@ -2,16 +2,30 @@ package com.eric.zvonilka.activities
 
 import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.eric.zvonilka.R
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DataSpec
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.RawResourceDataSource
+import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_launch.*
 
+
 class LaunchActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         supportActionBar?.hide()
         val animDrawable = root_launch_layout.background as AnimationDrawable
         animDrawable.setEnterFadeDuration(10)
@@ -21,8 +35,9 @@ class LaunchActivity : AppCompatActivity() {
             animDrawable.stop()
             launch()
             super.finish()
-        },2500)
+        }, 5000)
     }
+
     fun launch(){
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
--- app/src/main/java/com/eric/zvonilka/fragmentsNav/meets/MeetsFragment.kt
+++ app/src/main/java/com/eric/zvonilka/fragmentsNav/meets/MeetsFragment.kt
@@ -1,19 +1,74 @@
 package com.eric.zvonilka.fragmentsNav.meets
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.core.CameraSelector.LensFacing
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.eric.zvonilka.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
-import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.bottom_sheet.*
+import kotlinx.android.synthetic.main.fragment_meets.*
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.Mat
+import kotlin.math.abs
 
+typealias CornersListener = () -> Unit
 
 class MeetsFragment : Fragment() {
+
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+
+    private lateinit var safeContext: Context
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        safeContext = context
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isOffline = true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity).supportActionBar?.hide()
+        isOffline = false
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity).supportActionBar?.show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +76,7 @@ class MeetsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_meets, container, false)
+
         return root
     }
 
@@ -29,5 +85,118 @@ class MeetsFragment : Fragment() {
         val behavior: BottomSheetBehavior<*> = BottomSheetBehavior.from<View>(bottom_sheet)
         val initState = BottomSheetBehavior.STATE_COLLAPSED
         bottom_sheet.post(Runnable { behavior.state = initState })
+        behavior.addBottomSheetCallback(object: BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED)
+                    bottomSheet.setBackgroundResource(R.drawable.bottom_sheet_fullscreen_bg)
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED)
+                    bottomSheet.setBackgroundResource(R.drawable.bottom_sheet_bg)
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Toast.makeText(activity, "Oh its a me mario", Toast.LENGTH_LONG)
+            }
+
+        })
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+    private fun startCamera() {
+        OpenCVLoader.initDebug()
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(safeContext)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            preview = Preview.Builder().build()
+
+            imageCapture = ImageCapture.Builder().build()
+
+            imageAnalyzer = ImageAnalysis.Builder().build().apply {
+                setAnalyzer(Executors.newSingleThreadExecutor(), CornerAnalyzer {
+                    val bitmap = viewFinder.bitmap
+                    val img = Mat()
+                    Utils.bitmapToMat(bitmap, img)
+                    bitmap?.recycle()
+                    // Do image analysis here if you need bitmap
+                })
+            }
+            // Select back camera
+            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_FRONT).build()
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                camera = cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalyzer, preview, imageCapture)
+                preview!!.setSurfaceProvider(viewFinder.surfaceProvider)
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(safeContext))
+
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(safeContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(safeContext, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = activity?.externalMediaDirs?.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else activity?.filesDir!!
+    }
+
+
+    companion object {
+        val TAG = "CameraXFragment"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        internal const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        var isOffline = false // prevent app crash when goes offline
+    }
+
+    private class CornerAnalyzer(private val listener: CornersListener) : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        @SuppressLint("UnsafeExperimentalUsageError")
+        override fun analyze(imageProxy: ImageProxy) {
+            if (!isOffline) {
+                listener()
+            }
+            imageProxy.close() // important! if it is not closed it will only run once
+        }
+
     }
 }
+
